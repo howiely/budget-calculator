d83bc85459eaaf2fe516c37c3a56f20178ac51f8
howiely <howardly@student.ubc.ca>
2023-05-24 00:50:55 -0700

    Added multiplier variable + matching functionality

--- budget_calc.py
+++ budget_calc.py
@@ -4,32 +4,33 @@
 item_name = input("What item are you saving for?\n")
 cost = float(input("How much does it cost?\n"))
 current_savings = float(input("How much do you currently have saved?\n"))
-double_cost = cost * 2
+cost_multiplier = int(input("How many times more money do you want to save before you buy the item?\n"))
 
 # prompt - asks user for their information
 def prompt(item_name, cost, current_savings):
     print(item_name)
     print(cost)
     print(current_savings)
+    print(cost_multiplier)
 
     # total_progress - calculates how much they have saved (in percentage) rounded to 2 dec places
-    def total_progress(double_cost, current_savings):
-        float_total_progress = (current_savings/double_cost) * 100
+    def total_progress(cost_multiplier, current_savings):
+        float_total_progress = (current_savings/cost_multiplier) * 100
         total_progress = format(float_total_progress, '.2f')
         return total_progress
     total_progress(cost, current_savings)
 
-    # is_enough_money - checks whether current_savings is enough to fill double_cost
-    def is_enough_money(current_savings, double_cost):
-        current_savings < double_cost
-    is_enough_money(current_savings, double_cost)
+    # is_enough_money - checks whether current_savings is enough to fill cost_multiplier
+    def is_enough_money(current_savings, cost_multiplier):
+        current_savings < cost_multiplier
+    is_enough_money(current_savings, cost_multiplier)
 
     # total_remaining_cost - calculate how much more is needed to afford the item rounded to 2 dec places
-    def total_remaining_cost(current_savings, double_cost):
-        float_remaining_cost = double_cost - current_savings
+    def total_remaining_cost(current_savings, cost_multiplier):
+        float_remaining_cost = cost_multiplier - current_savings
         remaining_cost = format(float_remaining_cost, '.2f')
         return remaining_cost
-    total_remaining_cost(current_savings, double_cost)
+    total_remaining_cost(current_savings, cost_multiplier)
 
     # budget_cond - determines whether the user has enough money and prints a message telling them their progress
     def budget_cond():
@@ -37,8 +38,8 @@ def prompt(item_name, cost, current_savings):
             print("You have enough money to afford " + item_name)
         else:
             print("You don't have enough money for the " + item_name + " yet.\n" + 
-                  "You still need to save $" + str(total_remaining_cost(current_savings, double_cost)) +".")
-            print("Current Progress: " + str(total_progress(double_cost, current_savings)) + "%.")
+                  "You still need to save $" + str(total_remaining_cost(current_savings, cost_multiplier)) +".")
+            print("Current Progress: " + str(total_progress(cost_multiplier, current_savings)) + "%.")
     budget_cond()
 
 prompt(item_name, cost, current_savings)
